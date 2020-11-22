fa143b191b6ea2fbca829343fed4bd4a772f78f0
Jon Kuhn <jonkuhn@gmail.com>
2020-11-22 12:33:15 -0500

    Bootstrap OpenGL application with GLAD and GLFW to create a blank window.
    Up to the end of this page in the tutorial: https://learnopengl.com/Getting-started/Hello-Window

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,70 @@
 #include <vector>
 #include <string>
 
-using namespace std;
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // Make sure the viewport matches the new window dimensions
+    // NOTE: width and height will be significantly larger than specified
+    // on high resolution displays.
+    glViewport(0, 0, width, height);
+}
 
 int main()
 {
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
-
-    for (const string& word : msg)
+    if (glfwInit() != GLFW_TRUE)
     {
-        cout << word << " ";
+        std::cout << "Error: Failed to initialize glfw." << std::endl;
+        return 1;
     }
-    cout << endl;
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    auto window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return 1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return 1;
+    } 
+
+    while (!glfwWindowShouldClose(window))
+    {
+        processInput(window);
+        
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
