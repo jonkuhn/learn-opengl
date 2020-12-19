d5efe643741d3319a656ff445d27fc6a562d2028
Jon Kuhn <jonkuhn@gmail.com>
2020-12-19 16:22:14 -0500

    Add texture methods and GenVertexArrays to OpenGL wrapper.

--- src/GraphicsUnitTests/OpenGL/MockOpenGLWrapper.h
+++ src/GraphicsUnitTests/OpenGL/MockOpenGLWrapper.h
@@ -20,5 +20,12 @@ public:
     MOCK_METHOD(void, GetProgramiv, (GLuint program, GLenum pname, GLint *params), (override));
     MOCK_METHOD(void, GetProgramInfoLog, (GLuint program, GLsizei maxLength, GLsizei* length, GLchar* infoLog), (override));
     MOCK_METHOD(void, DeleteProgram, (GLuint program), (override));
+    MOCK_METHOD(void, GenVertexArrays, (GLsizei n, GLuint *arrays), (override));
+    MOCK_METHOD(void, GenTextures, (GLsizei n, GLuint* textures), (override));
+    MOCK_METHOD(void, DeleteTextures, (GLsizei n, const GLuint* textures), (override));
+    MOCK_METHOD(void, BindTexture, (GLenum target, GLuint texture), (override));
+    MOCK_METHOD(void, TexParameteri, (GLenum target, GLenum pname, GLint param), (override));
+    MOCK_METHOD(void, TexImage2D, (GLenum target, GLint level, GLint internalformat, GLsizei width,
+            GLsizei height, GLint border, GLenum format, GLenum type, const void * data), (override));
 };
 #pragma clang diagnostic pop
