a80f7327f7f79757f0810d1bb280fc6d5d622222
Jon Kuhn <jonkuhn@gmail.com>
2021-05-12 23:29:13 -0400

    Test move construction and move assignment for ShaderProgram

--- src/GraphicsUnitTests/OpenGL/ShaderProgramTests.cpp
+++ src/GraphicsUnitTests/OpenGL/ShaderProgramTests.cpp
@@ -40,6 +40,24 @@ protected:
         EXPECT_CALL(_mockLib, GetProgramiv(_, GL_LINK_STATUS, _)).WillOnce(SetArgPointee<2>(true));
         return std::make_unique<ShaderProgram>(&_mockLib, std::initializer_list<IShader*>({&_mockShaderA, &_mockShaderB}));
     }
+
+    std::tuple<std::unique_ptr<ShaderProgram>,
+        std::unique_ptr<ShaderProgram>> GetTwoShaderProgramsWithHandles(GLuint handleA, GLuint handleB)
+    {
+        Sequence seq;
+        EXPECT_CALL(_mockLib, CreateProgram()).InSequence(seq).WillOnce(Return(handleA));
+        EXPECT_CALL(_mockLib, GetProgramiv(_, GL_LINK_STATUS, _)).InSequence(seq).WillOnce(SetArgPointee<2>(true));
+        EXPECT_CALL(_mockLib, CreateProgram()).InSequence(seq).WillOnce(Return(handleB));
+        EXPECT_CALL(_mockLib, GetProgramiv(_, GL_LINK_STATUS, _)).InSequence(seq).WillOnce(SetArgPointee<2>(true));
+
+        auto programA = std::unique_ptr<ShaderProgram>(new ShaderProgram(&_mockLib, {&_mockShaderA, &_mockShaderB}));
+        auto programB = std::unique_ptr<ShaderProgram>(new ShaderProgram(&_mockLib, {&_mockShaderA, &_mockShaderB}));
+
+        Mock::VerifyAndClear(&_mockLib);
+
+        return std::tuple(std::move(programA), std::move(programB));
+    }
+
 };
 
 TEST_F(ShaderProgramTests, Constructor_MakesCallsToCreateAndLinkProgram)
@@ -122,6 +140,103 @@ TEST_F(ShaderProgramTests, Destructor_CallsDeleteProgram)
     {
         ShaderProgram shaderProgram(&_mockLib, {&_mockShaderA, &_mockShaderB});
     }
+}
+
+TEST_F(ShaderProgramTests, MoveConstruct_OnlySourceDoesNotCallDeleteProgram)
+{
+    EXPECT_CALL(_mockLib, CreateProgram()).WillOnce(Return(_testProgramHandle));
+    EXPECT_CALL(_mockLib, GetProgramiv(_, GL_LINK_STATUS, _)).WillOnce(SetArgPointee<2>(true));
+    EXPECT_CALL(_mockLib, DeleteProgram(_testProgramHandle)).Times(0);
+
+    std::unique_ptr<ShaderProgram> target;
+    {
+        ShaderProgram source(&_mockLib, {&_mockShaderA, &_mockShaderB});
+        target = std::make_unique<ShaderProgram>(std::move(source));
+    }
+    
+    // Explicitly verify before target destructor is automatically called
+    Mock::VerifyAndClear(&_mockLib);
+}
+
+TEST_F(ShaderProgramTests, MoveConstruct_TargetCallsDeleteProgram)
+{
+    EXPECT_CALL(_mockLib, CreateProgram()).WillOnce(Return(_testProgramHandle));
+    EXPECT_CALL(_mockLib, GetProgramiv(_, GL_LINK_STATUS, _)).WillOnce(SetArgPointee<2>(true));
+
+    std::unique_ptr<ShaderProgram> target;
+    {
+        ShaderProgram source(&_mockLib, {&_mockShaderA, &_mockShaderB});
+        target = std::make_unique<ShaderProgram>(std::move(source));
+    }
+
+    // Clear mock so we can specifically assert that the target destructor
+    // calls DeleteProgram
+    Mock::VerifyAndClear(&_mockLib);
+
+    EXPECT_CALL(_mockLib, DeleteProgram(_testProgramHandle)).Times(1);
+    target.reset();
+}
+
+TEST_F(ShaderProgramTests, MoveAssign_MoveDeletesTargetsOriginalProgram)
+{
+    const GLuint originalSourceHandle = 888;
+    const GLuint originalTargetHandle = 999;
+
+    std::unique_ptr<ShaderProgram> source, target;
+    std::tie(source, target) = GetTwoShaderProgramsWithHandles(originalSourceHandle, originalTargetHandle);
+
+    // Assert that target's original program is deleted when the move occurs
+    EXPECT_CALL(_mockLib, DeleteProgram(originalTargetHandle)).Times(1);
+    *target = std::move(*source);
+
+    // Explicitly check expectations so that other destructors do not get
+    // called before verification.
+    Mock::VerifyAndClear(&_mockLib);
+}
+
+TEST_F(ShaderProgramTests, MoveAssign_SourceDestructorDeletesNothing)
+{
+    const GLuint originalSourceHandle = 888;
+    const GLuint originalTargetHandle = 999;
+
+    std::unique_ptr<ShaderProgram> source, target;
+    std::tie(source, target) = GetTwoShaderProgramsWithHandles(originalSourceHandle, originalTargetHandle);
+
+    *target = std::move(*source);
+
+    // clear expectations so no previous calls affect our real assertion below
+    Mock::VerifyAndClear(&_mockLib);
+
+    // Assert that deleting the source does nothing
+    EXPECT_CALL(_mockLib, DeleteProgram(_)).Times(0);
+    source.reset();
+
+    // Explicitly check expectations so that other destructors do not get
+    // called before verification.
+    Mock::VerifyAndClear(&_mockLib);
+}
+
+TEST_F(ShaderProgramTests, MoveAssign_TargetDestructorDeletesSourceProgram)
+{
+    const GLuint originalSourceHandle = 888;
+    const GLuint originalTargetHandle = 999;
+
+    std::unique_ptr<ShaderProgram> source, target;
+    std::tie(source, target) = GetTwoShaderProgramsWithHandles(originalSourceHandle, originalTargetHandle);
+
+    *target = std::move(*source);
+
+    // clear expectations so no previous calls affect our real assertion below
+    Mock::VerifyAndClear(&_mockLib);
+
+    // Assert that the source's program (now in target) is deleted
+    // when target is destroyed
+    EXPECT_CALL(_mockLib, DeleteProgram(originalSourceHandle)).Times(1);
+    target.reset();
+
+    // Explicitly check expectations so that other destructors do not get
+    // called before verification.
+    Mock::VerifyAndClear(&_mockLib);
 }
 
 TEST_F(ShaderProgramTests, Use_CallsUseProgramWithHandle)
