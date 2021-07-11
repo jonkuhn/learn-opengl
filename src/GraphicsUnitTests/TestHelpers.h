3c0f77c4680c524531c0a6eff3a0aa65f85b36bb
Jon Kuhn <jonkuhn@gmail.com>
2021-07-11 00:13:39 -0400

    fixup! Create ObjectInstance2d and Camera2d classes

--- src/GraphicsUnitTests/TestHelpers.h
+++ src/GraphicsUnitTests/TestHelpers.h
@@ -2,6 +2,14 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-volatile"
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/string_cast.hpp>
+#pragma clang diagnostic pop
+
 MATCHER_P(DoublePtrStrEq, expectedStr, "") { return expectedStr == std::string(*arg); }
 
 ACTION_TEMPLATE(FillArgPointeeBuffer, HAS_1_TEMPLATE_PARAMS(unsigned, argIndex), AND_2_VALUE_PARAMS(srcBuffer, srcSize))
@@ -12,4 +20,12 @@ ACTION_TEMPLATE(FillArgPointeeBuffer, HAS_1_TEMPLATE_PARAMS(unsigned, argIndex),
 ACTION_TEMPLATE(SavePointerToArgReferee, HAS_1_TEMPLATE_PARAMS(unsigned, argIndex), AND_1_VALUE_PARAMS(pointer))
 {
     *pointer = &(std::get<argIndex>(args));
+}
+
+inline void ExpectEqual(const glm::mat4& actual, const glm::mat4& expected)
+{
+    EXPECT_TRUE(0 == std::memcmp(glm::value_ptr(actual), glm::value_ptr(expected), 4 * 4 * sizeof(float)))
+        << "Matrices are not equal" << std::endl
+        << "actual:" << glm::to_string(actual) << std::endl
+        << " expected:" << glm::to_string(expected);
 }
